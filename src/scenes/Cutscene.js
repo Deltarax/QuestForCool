9be6a03b06080fbf6452cfa293983a30f5512e0f
Jackson <deltarax@gmail.com>
2022-05-11 18:17:22 -0700

    Prototype cutscene!

--- src/scenes/Cutscene.js
+++ src/scenes/Cutscene.js
@@ -5,13 +5,24 @@ class Cutscene extends Phaser.Scene {
 
     create() {
 
+        // Event Flags
+        this.movingFlag = true;
+        this.speechFlag = false;
+        this.teardropFlag = false;
+        this.finishedFlag = false;
+
+        this.sceneTicks = 0;
+
         // added keycode
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
         // checks what kind of cutscene we are in, and puts appropriate text
         if (cutsceneState == 'start'){
-            console.log('here');
-            this.add.text(0, game.config.height/2, "press space to start", textConfig);
+            this.mainGuy = this.add.sprite(100, 150, 'cutsceneMC').setOrigin(0.5,0.5);
+            this.coolKids = this.add.sprite(700, 500, 'cutsceneCoolKids').setOrigin(0.5,0.5);
+            this.speechBubble = this.add.sprite(800, 300, 'weightSpeechBubble').setOrigin(0.5,0.5).setAlpha(0);
+            this.teardrop = this.add.sprite(280, 50, 'teardrop').setOrigin(0.5,0.5).setAlpha(0);
+            // this.add.text(0, game.config.height/2, "press space to start", textConfig);
         } else if (cutsceneState == 'end'){
             this.add.text(0, game.config.height/2, "you are STRONG!", textConfig);
         }
@@ -19,11 +30,44 @@ class Cutscene extends Phaser.Scene {
 
     }
 
-    // If we are in the beginning, press start to play!
     update() {
+
+        // if the cutscene is the start one
         if (cutsceneState == 'start'){
-            if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
-                this.scene.start("weightliftScene");
+            // MOving segment
+            if (this.movingFlag){
+                if (this.mainGuy.x < 300){
+                    this.mainGuy.x++;    //move the mc
+                } else {
+                    // change part of scene
+                    this.movingFlag = false;
+                    this.speechFlag = true;
+                }
+            } else if (this.speechFlag){
+                // reveal the speachbubble
+                this.speechBubble.setAlpha(1);
+                this.sceneTicks++;
+                // after one sec, change part of scene
+                if (this.sceneTicks > 100){
+                    this.speechFlag = false;
+                    this.teardropFlag = true;
+                }
+            } else if (this.teardropFlag){
+                // reveal teardrop
+                this.teardrop.setAlpha(1);
+                // make teardrop fall down face
+                if (this.teardrop.y < 100){
+                    this.teardrop.y++;
+                } else {
+                    this.teardropFlag = false;
+                    this.finishedFlag = true;
+                }
+            } else if (this.finishedFlag) {
+                // allow player to press space to begin!
+                this.add.text(0, game.config.height/2, "press space to start", textConfig);
+                if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
+                    this.scene.start("weightliftScene");
+                }
             }
         }
     }
--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -6,7 +6,11 @@ class Load extends Phaser.Scene {
     preload(){
         this.load.image('weightliftDown', './assets/tempWeightliftDown.png');
         this.load.image('weightliftUp', './assets/tempWeightliftUp.png');
+        this.load.image('cutsceneMC', './assets/tempCutsceneMC.png');
+        this.load.image('cutsceneCoolKids', './assets/tempCoolKids.png');
         this.load.image('arrow', './assets/arrow.png');
+        this.load.image('weightSpeechBubble', './assets/tempWeightSpeech.png');
+        this.load.image('teardrop', './assets/tempTeardrop.png');
     }
 
     update(){
