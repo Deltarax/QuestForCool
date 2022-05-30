472e5059e1167099792f99dddd789f5ae11ad256
sbell9724 <somibell@ucsc.edu>
2022-05-30 10:23:19 -0700

    hurdle completed

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -31,7 +31,7 @@ class Load extends Phaser.Scene {
 
         this.load.image('hurdle', './assets/tempHurdle.png'); //temp
         this.load.image('hurdleRun', './assets/hurdleRun.png');
-        this.load.image('hurdleJump', './assets/hurdleJump');
+        this.load.image('hurdleJump', './assets/hurdleJump.png');
         this.load.image('hurdleBG', './assets/tempHurdleBG.png'); //temp, might leave
 
         // music and sound
